e25313df0df74ccd68fae43278fa564f5b992ae7
Donking101 <versaliteworld@gmail.com>
2023-08-21 00:42:27 +0100

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,17 @@
 #include <unistd.h>
 
-int main(void) {
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a specific message to the standard error.
+ * The message is "and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line.
+ *
+ * Return: Always 1 (error code)
+ */
+int main(void)
+{
     char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, 59);
+    write(2, message, 59); // Using file descriptor 2 for standard error
     return 1;
 }
