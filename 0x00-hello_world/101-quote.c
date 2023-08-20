d2889191849fdd46ac113a1f6e6cf0f275921ba4
Donking101 <versaliteworld@gmail.com>
2023-08-21 00:45:23 +0100

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <unistd.h>
 
 /**
  * main - Entry point of the program
@@ -13,5 +14,6 @@ int main(void)
 {
     char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     write(2, message, 59);
-    return 1;
+    return (1);
 }
+
