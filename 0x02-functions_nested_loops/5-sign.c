10517f930192163954d524f0ae0c3b0ba16db5d3
Donking101 <versaliteworld@gmail.com>
2023-08-25 01:27:06 +0100

    corrections and update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,7 +13,7 @@ int print_sign(int n)
 	{
 		_putchar(43);
 		return (1);
-	} 
+	}
 	else if (n < 0)
 	{
 		_putchar(45);
@@ -22,7 +22,7 @@ int print_sign(int n)
 	else
 	{
 		_putchar(48);
-		return(0);
+		return (0);
 	}
 	_putchar('\n');
 }
